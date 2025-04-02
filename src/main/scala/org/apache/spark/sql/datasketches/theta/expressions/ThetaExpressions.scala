0ed6a12cd4de14b059bc797c8a74add0df433e23
AlexanderSaydakov <AlexanderSaydakov@users.noreply.github.com>
2025-04-01 23:40:22 -0700

    attempt to fix codegen

--- src/main/scala/org/apache/spark/sql/datasketches/theta/expressions/ThetaExpressions.scala
+++ src/main/scala/org/apache/spark/sql/datasketches/theta/expressions/ThetaExpressions.scala
@@ -63,11 +63,12 @@ case class ThetaSketchGetEstimate(child: Expression)
     val sketch = ctx.freshName("sketch")
     val code = s"""
       ${childEval.code}
-      if (${childEval.isNull}) {
-        ${ev.value} = null;
-      } else {
-        final org.apache.datasketches.theta.Sketch $sketch = org.apache.spark.sql.datasketches.theta.types.ThetaSketchWrapper.wrapAsReadOnlySketch(${childEval.value});
-        ${ev.value} = $sketch.getEstimate();
+      final boolean ${ev.isNull} = ${childEval.isNull};
+      double ${ev.value} = 0.0;
+      if (!${ev.isNull}) {
+        final org.apache.datasketches.theta.Sketch $$sketch =
+          org.apache.spark.sql.datasketches.theta.types.ThetaSketchWrapper.wrapAsReadOnlySketch(${childEval.value});
+        ${ev.value} = $$sketch.getEstimate();
       }
     """
     ev.copy(code = CodeBlock(Seq(code), Seq.empty), isNull = childEval.isNull)
